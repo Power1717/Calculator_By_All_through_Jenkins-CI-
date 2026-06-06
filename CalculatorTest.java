3facc6f305b5774aac84872982d7fe36a009b9ef
Saurabh Pawar <powerpawar17@gmail.com>
2026-06-06 20:21:01 +0530

    File Saved

---
+++ CalculatorTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(15, calculator.add(10, 5));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(50, calculator.multiply(10, 5));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calculator.divide(10, 5));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> calculator.divide(10, 0));
+
+        assertEquals(
+                "Division by zero is not allowed",
+                exception.getMessage());
+    }
+}
